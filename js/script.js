8469e2f558bbe2052948fa0a9c9fce0d32b678bf
mrcjshy <joshmaravilla891@gmail.com>
2025-11-03 22:43:12 +0800

    working on navbar

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const carouselInner = document.querySelector('.carousel-inner');
+let slides = document.querySelectorAll('.carousel-inner .slide');
+let index = 0;
+
+
+const firstClone = slides[0].cloneNode(true);
+carouselInner.appendChild(firstClone);
+slides = document.querySelectorAll('.carousel-inner .slide'); 
+
+function showNextSlide() {
+  index++;
+  carouselInner.style.transition = 'transform 1s ease-in-out';
+  carouselInner.style.transform = `translateX(-${index * 100}%)`;
+
+  if (index === slides.length - 1) {
+    setTimeout(() => {
+      carouselInner.style.transition = 'none';
+      carouselInner.style.transform = 'translateX(0)';
+      index = 0;
+    }, 1000); 
+  }
+}
+
+setInterval(showNextSlide, 2000);
